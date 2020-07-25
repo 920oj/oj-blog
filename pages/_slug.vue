1d7bed787c35303386a1251a0e207bbb37476d34
Junya Oiwa <0920oiwa@gmail.com>
2020-07-25 23:32:48 +0900

    Initial Commit

---
+++ pages/_slug.vue
@@ -0,0 +1,29 @@
+<template>
+  <article>
+    <h1>{{data.title}}</h1>
+    <nuxt-content :document="data"></nuxt-content>
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const data = await $content('/', params.slug).fetch()
+    return { data }
+  },
+  mounted() {
+    console.log(this.data)
+  },
+  head() {
+    return {
+      title: this.data.title,
+      meta: [
+        { hid: 'description', name: 'description', content: this.data.title },
+      ],
+    }
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="index"></div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>

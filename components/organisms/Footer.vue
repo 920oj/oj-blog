e6b5fce64410fea3855ae5a2267d3710a0b418cd
Junya Oiwa <0920oiwa@gmail.com>
2020-07-26 23:23:27 +0900

    [add]Header・Footerを作成

---
+++ components/organisms/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="footer bg-gray-800">
+    <p class="text-center py-16 text-white">&copy; 2020 Junya Oiwa(@920OJ)</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
---
+++ components/organisms/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header bg-white shadow-lg">
+    <Snslink />
+    <div class="header-title text-center py-8">
+      <h1 class="text-3xl font-bold">OJ Blog</h1>
+      <p class="text-sm my-4">おーじぇいのブログ</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Snslink from '~/components/molecules/Sns-link.vue'
+export default {
+  components: {
+    Snslink,
+  },
+}
+</script>
+
+<style>
+</style>

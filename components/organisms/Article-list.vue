59fbf21ca63e19d4f9c3b4c6b526229f03106c23
Junya Oiwa <0920oiwa@gmail.com>
2020-08-01 01:52:26 +0900

    [add]次ページ読み込みを実装

--- components/organisms/Article-list.vue
+++ components/organisms/Article-list.vue
@@ -12,6 +12,12 @@
       :slug="elem.slug"
       :createdAt="elem.createdAt"
     />
+    <div class="text-center py-4" v-if="!isAllLoaded">
+      <div
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-block cursor-pointer"
+        @click="loadPage(articleNumber)"
+      >さらに読み込む</div>
+    </div>
   </div>
 </template>
 
@@ -21,24 +27,38 @@ import ArticleBox from '~/components/molecules/Article-box.vue'
 export default {
   data() {
     return {
-      article_number: 0,
+      all_article_number: 0,
       article_list: [],
+      now_page: 1,
     }
   },
   computed: {
+    articleNumber() {
+      return this.article_list.length
+    },
     pageNumber() {
-      return Math.floor(this.article_number / 10) + 1
+      return Math.ceil(this.all_article_number / 10)
+    },
+    isAllLoaded() {
+      return this.now_page == this.pageNumber
     },
   },
   components: {
     ArticleBox,
+  },
+  methods: {
+    async loadPage(skip) {
+      const add_data = await fetchData.getArticleList(this.$content, skip, 10)
+      this.article_list = this.article_list.concat(add_data)
+      this.now_page += 1
+    },
   },
   async fetch() {
     await Promise.all([
       fetchData.getArticleNumber(this.$content),
       fetchData.getArticleList(this.$content, 0, 10),
     ]).then((values) => {
-      this.article_number = values[0]
+      this.all_article_number = values[0]
       this.article_list = values[1]
     })
   },
@@ -49,6 +69,7 @@ export default {
 @media (min-width: 1024px) {
   .article-list {
     flex: 5;
+    height: fit-content;
   }
 }
 </style>
